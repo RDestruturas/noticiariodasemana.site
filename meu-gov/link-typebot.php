b4f68fa8429d626300dbd6532a1757c925296d45
RDestruturas <146151067+RDestruturas@users.noreply.github.com>
2024-02-10 15:26:16 -0300

    link-typebot.php

--- meu-gov/link-typebot.php
+++ meu-gov/link-typebot.php
@@ -3,6 +3,6 @@
 // Defina o link do funil do type
 // Ex: https://chat.typebot/gov-atendimento
 
-$linkType = "https://chat.descomplicandoavida.shop/gov-atendimento";
+$linkType = "https://suanoticiadasemana.site/consulta23";
 
 ?>
